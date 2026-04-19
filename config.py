ecd8b2772c2ed931274f97527c1a419a057d3105
Avisha Gupta <avishagupta1290@gmail.com>
2026-04-19 23:08:25 +0530

    gitignore

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "api_openai"
 
 MODEL_NAME = "gpt-4o-mini"
 EMBEDDING_MODEL = "text-embedding-3-small"
 
-QDRANT_URL="https://970ff56e-362f-4d27-82ad-d21061f910e4.eu-central-1-0.aws.cloud.qdrant.io"
-QDRANT_API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6YzI4MTZiNWEtOGQ4Zi00ZGRkLWFlMmYtYThjM2ZiYjA1ZDk5In0.Z7j38MtnpDcxviiX_G86DgdwkZx-lVlwldGm8AZeYfI"
+QDRANT_URL="link"
+QDRANT_API_KEY="api_qdrant"
